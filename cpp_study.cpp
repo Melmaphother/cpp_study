d84af15beb52b5c1fa27e7c03fc1eda5141a168c
Melmaphother <1559792558@qq.com>
2023-07-11 20:53:09 +0800

    modify cpp_study

--- cpp_study.cpp
+++ cpp_study.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
 #include <Windows.h>
+#include <iostream>
+#include <string>
 using namespace std;
 /* 函数模板
 template<typename T>
 void myPrint(T a, T b){
-    std::cout << "transfer function template" << std::endl;
+	std::cout << "transfer function template" << std::endl;
 }
 myPrint<int>(a, b);
 */
@@ -16,26 +17,34 @@ public     类内可以访问，类外可以访问
 private    类内可以访问，类外不可以访问，子类不可以访问父类中的私有内容
 protected  类内可以访问，类外不可以访问，子类可以访问父类中的保护内容
 */
-class Circle{
-    // 访问权限
-    // 公共权限
+class Circle {
+	// 访问权限
+	// 公共权限
 public:
-    // 类中的属性和行为统称为成员
-    // 属性
-    // 半径
-    int r;
-    // 行为
-    // 求周长
-    double calculateZC(){
-        return 2 * PI * r;
-    }
-
+	// 类中的属性和行为统称为成员
+	// 属性
+	// 半径
+	int r;
+	// 行为
+	// 求周长
+	double calculateZC() { return 2 * PI * r; }
 };
 
-int main(){ 
-    // 通过圆类 创建一个具体的圆（对象）
-    Circle c1;
-    c1.r = 10;
-    cout << c1.calculateZC()<< endl;
-    system("pause");
+class Person {
+public:
+	void   SetName(int name) { m_Name = name; }
+	string GetName() { return m_Name; }
+
+private:
+	string m_Name;
+	int	   m_Age;
+	string m_lover;
+};
+
+int main() {
+	// 通过圆类 创建一个具体的圆（对象）
+	Circle c1;
+	c1.r = 10;
+	cout << c1.calculateZC() << endl;
+	system("pause");
 }
--- cpp_study.md
+++ cpp_study.md
@@ -6,6 +6,22 @@
   - public
   - protected
   - private
+    成员属性的私有化，可以自己控制读写权限，检测有效性
+    ```cpp
+        class Person{
+        public:
+            void SetName(int name){
+                m_Name = name;
+            }
+            string GetName(){
+                return m_Name;
+            }
+        private: 
+            string m_Name;
+            int m_Age;
+            string m_lover;
+        };
+    ```
 - 继承
 - 多态
 
