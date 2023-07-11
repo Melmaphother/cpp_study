23255e60c3305ca5a9c905c23fb756e2d75453be
Melmaphother <1559792558@qq.com>
2023-07-11 19:17:43 +0800

    modify cpp_study

--- cpp_study.cpp
+++ cpp_study.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <Windows.h>
 
+/* 函数模板
 template<typename T>
 void myPrint(T a, T b){
     std::cout << "transfer function template" << std::endl;
 }
-
+myPrint<int>(a, b);
+*/
 
 
 int main(){ 
     int a, b;
-    myPrint<int>(a, b);
     system("pause");
 }
